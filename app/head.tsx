6466d731ba210f0b0a853f7014453d933a3d971c
Ranziroo <ajunf2@gmail.com>
2025-12-17 20:36:37 +0700

    new

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>Luxoria</title>
+      <link rel="icon" href="https://img.pikbest.com/png-images/20241101/-22golden-royalty-luxury-logo-22_11041756.png!sw800" />
+    </>
+  );
+}
